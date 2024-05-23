60e1eebb184f282f27ea55f6af925d435c8dffe9
someview <fengyeall@gmail.com>
2024-05-23 18:45:35 +0800

    fix:添加测试代码

--- go.mod
+++ go.mod
@@ -2,4 +2,3 @@ module github.com/someview/go-metrics
 
 go 1.22.0
 
-require github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475
--- histogram.go
+++ histogram.go
@@ -1,5 +1,7 @@
 package metrics
 
+import . "github.com/someview/go-metrics/sample"
+
 // Histograms calculate distribution statistics from a series of int64 values.
 type Histogram interface {
 	Clear()
@@ -103,50 +105,6 @@ func (*HistogramSnapshot) Update(int64) {
 
 // Variance returns the variance of inputs at the time the snapshot was taken.
 func (h *HistogramSnapshot) Variance() float64 { return h.sample.Variance() }
-
-// NilHistogram is a no-op Histogram.
-type NilHistogram struct{}
-
-// Clear is a no-op.
-func (NilHistogram) Clear() {}
-
-// Count is a no-op.
-func (NilHistogram) Count() int64 { return 0 }
-
-// Max is a no-op.
-func (NilHistogram) Max() int64 { return 0 }
-
-// Mean is a no-op.
-func (NilHistogram) Mean() float64 { return 0.0 }
-
-// Min is a no-op.
-func (NilHistogram) Min() int64 { return 0 }
-
-// Percentile is a no-op.
-func (NilHistogram) Percentile(p float64) float64 { return 0.0 }
-
-// Percentiles is a no-op.
-func (NilHistogram) Percentiles(ps []float64) []float64 {
-	return make([]float64, len(ps))
-}
-
-// Sample is a no-op.
-func (NilHistogram) Sample() Sample { return NilSample{} }
-
-// Snapshot is a no-op.
-func (NilHistogram) Snapshot() Histogram { return NilHistogram{} }
-
-// StdDev is a no-op.
-func (NilHistogram) StdDev() float64 { return 0.0 }
-
-// Sum is a no-op.
-func (NilHistogram) Sum() int64 { return 0 }
-
-// Update is a no-op.
-func (NilHistogram) Update(v int64) {}
-
-// Variance is a no-op.
-func (NilHistogram) Variance() float64 { return 0.0 }
 
 // StandardHistogram is the standard implementation of a Histogram and uses a
 // Sample to bound its memory use.
